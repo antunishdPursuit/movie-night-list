9f9a38011745b76efebe79420035c39ade77b3a9
antunishdPursuit <dennysantunish@pursuit.org>
2023-08-08 20:12:10 -0400

    added routed for crud functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./Pages/Home";
+import ErrorPage from "./Pages/ErrorPage";
+import Show from "./Pages/Show";
+import Index from "./Pages/Index";
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Index />} />
+            <Route path="/movies/new" element={<New />} />
+            <Route path="/movies/:index" element={<Show />} />
+            <Route path="/movies/:index/edit" element={<Edit />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
